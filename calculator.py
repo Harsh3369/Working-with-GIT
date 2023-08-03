20454a3d0a8c88186e697e63bbe3567899223947
Harshit Sundriyal <31472849+Harsh3369@users.noreply.github.com>
2023-08-03 11:11:09 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,23 +1,26 @@
-def calculator(x, y, operation):
-  """
-  This function performs the specified operation on two numbers.
+# def calculator(x, y, operation):
+#   """
+#   This function performs the specified operation on two numbers.
 
-  Args:
-    x: The first number.
-    y: The second number.
-    operation: The operation to perform.
+#   Args:
+#     x: The first number.
+#     y: The second number.
+#     operation: The operation to perform.
 
-  Returns:
-    The result of the operation.
-  """
+#   Returns:
+#     The result of the operation.
+#   """
 
-  if operation == "add":
-    return x + y
-  elif operation == "subtract":
-    return x - y
-  elif operation == "multiply":
-    return x * y
-  elif operation == "divide":
-    return x / y
-  else:
-    raise ValueError("Invalid operation")
+#   if operation == "add":
+#     return x + y
+#   elif operation == "subtract":
+#     return x - y
+#   elif operation == "multiply":
+#     return x * y
+#   elif operation == "divide":
+#     return x / y
+#   else:
+#     raise ValueError("Invalid operation")
+
+def cal_add(x,y):
+  return x+y
