3ea3895243ffe36856f9610184de08d9bef756d3
Harshit Sundriyal <31472849+Harsh3369@users.noreply.github.com>
2023-08-03 11:41:59 +0530

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -2,42 +2,39 @@ import pytest
 
 import calculator
 
-class calc_test():
-   def test_add(self):
-     assert 6 == calculator.cal_add(3,3)
 
-# class calc_test():
-#   def test_add(self):
-#     """
-#     This test ensures that the calculator function correctly adds two numbers.
-#     """
+class calc_test():
+  def test_add(self):
+    """
+    This test ensures that the calculator function correctly adds two numbers.
+    """
   
-#     result = calculator(10, 20, "add")
-#     assert result == 30
-  
-  
-#   def test_subtract(self):
-#     """
-#     This test ensures that the calculator function correctly subtracts two numbers.
-#     """
-  
-#     result = calculator(10, 20, "subtract")
-#     assert result == -10
+    result = calculator(10, 20, "add")
+    assert result == 30
   
   
-#   def test_multiply(self):
-#     """
-#     This test ensures that the calculator function correctly multiplies two numbers.
-#     """
+  def test_subtract(self):
+    """
+    This test ensures that the calculator function correctly subtracts two numbers.
+    """
   
-#     result = calculator(10, 20, "multiply")
-#     assert result == 200
+    result = calculator(10, 20, "subtract")
+    assert result == -10
   
   
-#   def test_divide(self):
-#     """
-#     This test ensures that the calculator function correctly divides two numbers.
-#     """
+  def test_multiply(self):
+    """
+    This test ensures that the calculator function correctly multiplies two numbers.
+    """
   
-#     result = calculator(10, 20, "divide")
-#     assert result == 0.5
+    result = calculator(10, 20, "multiply")
+    assert result == 200
+  
+  
+  def test_divide(self):
+    """
+    This test ensures that the calculator function correctly divides two numbers.
+    """
+  
+    result = calculator(10, 20, "divide")
+    assert result == 0.5
